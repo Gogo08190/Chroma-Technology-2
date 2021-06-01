a650258407f37b4c49ad7aadeeae49f76bf33566
mathieu <mathieucrombe@gmail.com>
2021-06-01 23:08:16 +0200

    modification craft hammer immersive

--- kubejs/server_scripts/recipes.js
+++ kubejs/server_scripts/recipes.js
@@ -207,5 +207,14 @@ onEvent('recipes', event => {
     C: 'minecraft:cobblestone_wall'
   });
   //craft hammer immersive
-  
+  event.remove({ output: 'immersiveengineering:hammer' })
+  event.recipes.create.mechanical_crafting(Item.of('immersiveengineering:hammer'),[
+    ' IP',
+    ' SI',
+    'S  '
+  ], {
+    P: 'create:powdered_obsidian',
+    S: 'createaddition:copper_rod',
+    I: 'create:iron_sheet'
+  })
 });
