d699d29287adaee291a8625a4356bf6125a41f7a
Gogo08 <gogo08190@gmail.com>
2021-06-07 17:44:42 +0200

    Pneumaticraft craft

--- kubejs/server_scripts/mekanism/recipes.js
+++ kubejs/server_scripts/mekanism/recipes.js
@@ -2,13 +2,13 @@ onEvent('recipes', event => {
 
   // Steel Casing
   event.remove({ output: 'mekanism:steel_casing' })
-  event.recipes.create.mechanical_crafting(Item.of('mekanism:steel_casing'),[
+  event.shaped('mekanism:steel_casing', [
     'SGC',
     'GOG',
     'CGS'
   ], {
     G: 'thermal:obsidian_glass',
-    S: 'mekanism:ingot_osmium'
+    S: '#forge:ingots/steel',
     O: 'mekanism:ingot_osmium',
     C: 'pneumaticcraft:ingot_iron_compressed'
 
