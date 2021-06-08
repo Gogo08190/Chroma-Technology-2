45f6423ec3c6f9cc3ab2f719f4f522adcfbf2490
mathieu <mathieucrombe@gmail.com>
2021-06-08 19:38:52 +0200

    changement craft applied (cell et processor)

---
+++ kubejs/server_scripts/applied/recipes.js
@@ -0,0 +1,84 @@
+onEvent('recipes', event => {
+    //craft energy cell
+    event.remove({ output: 'appliedenergistics2:energy_cell' })
+    event.shaped('appliedenergistics2:energy_cell', [
+        'PHP',
+        'DAD',
+        'PHP'
+    ], {
+        D: 'powah:dielectric_rod',
+        A: 'mekanism:alloy_atomic',
+        P: 'appliedenergistics2:purified_certus_quartz_crystal',
+        H: 'powah:dielectric_rod_horizontal'
+    })
+
+    //Enginering processor
+    event.remove({ output: 'appliedenergistics2:engineering_processor' })
+    event.custom(
+        {
+          "type": "appliedenergistics2:inscriber",
+          "mode": "press",
+          "result": {
+            "item": "appliedenergistics2:engineering_processor"
+          },
+          "ingredients": {
+            "top": {
+              "item": "appliedenergistics2:printed_engineering_processor"
+            },
+            "middle": {
+              "tag": "refinedstorage:construction_core"
+            },
+            "bottom": {
+              "item": "appliedenergistics2:printed_silicon"
+            }
+          }
+        }
+    )
+    
+    //Logic processor    
+    event.remove({ output: 'appliedenergistics2:logic_processor' })
+    event.custom(
+        {
+          "type": "appliedenergistics2:inscriber",
+          "mode": "press",
+          "result": {
+            "item": "appliedenergistics2:logic_processor"
+          },
+          "ingredients": {
+            "top": {
+              "item": "appliedenergistics2:printed_logic_processor"
+            },
+            "middle": {
+              "tag": "refinedstorage:destruction_core"
+            },
+            "bottom": {
+              "item": "appliedenergistics2:printed_silicon"
+            }
+          }
+        }
+    )
+    
+    //Calculation processor    
+    event.remove({ output: 'appliedenergistics2:calculation_processor' })
+    event.custom(
+        {
+          "type": "appliedenergistics2:inscriber",
+          "mode": "press",
+          "result": {
+            "item": "appliedenergistics2:calculation_processor"
+          },
+          "ingredients": {
+            "top": {
+              "item": "appliedenergistics2:printed_calculation_processor"
+            },
+            "middle": {
+              "tag": "refinedstorage:construction_core"
+            },
+            "bottom": {
+              "item": "appliedenergistics2:printed_silicon"
+            }
+          }
+        }
+    )
+
+});
