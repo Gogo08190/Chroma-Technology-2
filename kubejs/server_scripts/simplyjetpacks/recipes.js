0741040d7cf2fbad7be66637927c79b3b09aa223
Gogo08 <gogo08190@gmail.com>
2021-06-14 17:50:32 +0200

    Fix

--- kubejs/server_scripts/simplyjetpacks/recipes.js
+++ kubejs/server_scripts/simplyjetpacks/recipes.js
@@ -24,38 +24,39 @@ onEvent('recipes', event => {
     I: '#forge:ingots/electrum'
   })
 
-    // Resonant Thruster
-    event.shaped('simplyjetpacks:thruster_te4', [
+  // Resonant Thruster
+  event.shaped('simplyjetpacks:thruster_te4', [
+  'III',
+  'CBC',
+  'CRC'
+  ], {
+    C: 'powah:energy_cable_niotic',
+    R: 'silents_mechanisms:redstone_alloy_block',
+    B: 'powah:battery_niotic',
+    I: '#forge:ingots/enderium'
+  })
+
+  // Fluxed Thruster
+  event.shaped('simplyjetpacks:thruster_te5', [
+  'III',
+  'CBC',
+  'CRC'
+  ], {
+    C: 'powah:energy_cable_nitro',
+    R: 'silents_mechanisms:redstone_alloy_block',
+    B: 'powah:battery_nitro',
+    I: '#forge:ingots/lumium'
+  })
+
+    // Leadstone Thruster
+    event.shaped('simplyjetpacks:thruster_te1', [
     'III',
     'CBC',
     'CRC'
     ], {
-      C: 'powah:energy_cable_niotic',
+      C: 'powah:energy_cable_starter',
       R: 'silents_mechanisms:redstone_alloy_block',
-      B: 'powah:battery_niotic',
-      I: '#forge:ingots/enderium'
+      B: 'powah:battery_starter',
+      I: '#forge:ingots/lead'
     })
-
-      // Fluxed Thruster
-      event.shaped('simplyjetpacks:thruster_te5', [
-      'III',
-      'CBC',
-      'CRC'
-      ], {
-        C: 'powah:energy_cable_nitro',
-        R: 'silents_mechanisms:redstone_alloy_block',
-        B: 'powah:battery_nitro',
-        I: '#forge:ingots/lumium'
-      })
-
-        // Leadstone Thruster
-        event.shaped('simplyjetpacks:thruster_te1', [
-        'III',
-        'CBC',
-        'CRC'
-        ], {
-          C: 'powah:energy_cable_starter',
-          R: 'silents_mechanisms:redstone_alloy_block',
-          B: 'powah:battery_starter',
-          I: '#forge:ingots/lead'
 });
