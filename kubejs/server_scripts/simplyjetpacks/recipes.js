1bb9f958795ecca4aa5d93448324f3eb71129e43
Gogo08 <gogo08190@gmail.com>
2021-06-14 17:05:38 +0200

    Simply Craft

---
+++ kubejs/server_scripts/simplyjetpacks/recipes.js
@@ -0,0 +1,26 @@
+onEvent('recipes', event => {
+
+  // Hardened Thruster
+  event.shaped('simplyjetpacks:thruster_te2', [
+  'III',
+  'CBC',
+  'CRC'
+  ], {
+    C: 'powah:energy_cable_basic',
+    R: 'silents_mechanisms:redstone_alloy_block',
+    B: 'powah:battery_basic',
+    I: '#forge:ingots/invar'
+  })
+
+  // Hardened Thruster
+  event.shaped('simplyjetpacks:thruster_te2', [
+  'III',
+  'CBC',
+  'CRC'
+  ], {
+    C: 'powah:energy_cable_basic',
+    R: 'silents_mechanisms:redstone_alloy_block',
+    B: 'powah:battery_basic',
+    I: '#forge:ingots/invar'
+  })
+});
