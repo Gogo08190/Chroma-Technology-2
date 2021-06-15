d16251efec099a028f5aa684868725a5064d550f
Victor <lerouxvictor3@gmail.com>
2021-06-15 20:38:23 +0200

    Added craft of the mekasuit

--- kubejs/server_scripts/mekanism/recipes.js
+++ kubejs/server_scripts/mekanism/recipes.js
@@ -13,4 +13,49 @@ onEvent('recipes', event => {
     C: 'pneumaticcraft:ingot_iron_compressed'
 
   })
+
+  // MekaSuit Boots
+  event.remove({ output: 'mekanism:mekasuit_boots'})
+  event.shaped('mekanism:mekasuit_boots', [
+    'HUH',
+    'HBH',
+    'PIP'
+  ], {
+    H: 'mekanism:hdpe_sheet',
+    U: 'mekanism:ultimate_control_circuit',
+    B: 'upgradednetherite:ultimate_upgraded_netherite_boots',
+    I: 'mekanism:ultimate_induction_cell',
+    P: 'mekanism:pellet_polonium'
+
+  })
+
+  //MekaSuit Leggings
+  event.remove({ output: 'mekanism:mekasuit_leggings'})
+  event.shaped('mekanism:mekasuit_leggings', [
+    'HUH',
+    'HLH',
+    'PIP'
+  ], {
+    H: 'mekanism:hdpe_sheet',
+    U: 'mekanism:ultimate_control_circuit',
+    L: 'upgradednetherite:ultimate_upgraded_netherite_leggings',
+    I: 'mekanism:ultimate_induction_cell',
+    P: 'mekanism:pellet_polonium'
+
+  })
+
+  //MekaSuit Helmet
+  event.remove({ output: 'mekanism:mekasuit_helmet'})
+  event.shaped('mekanism:mekasuit_helmet', [
+    'HUH',
+    'HNH',
+    'PIP'
+  ], {
+    H: 'mekanism:hdpe_sheet',
+    U: 'mekanism:ultimate_control_circuit',
+    N: 'upgradednetherite:ultimate_upgraded_netherite_helmet',
+    I: 'mekanism:ultimate_induction_cell',
+    P: 'mekanism:pellet_polonium'
+
+  })
 });
