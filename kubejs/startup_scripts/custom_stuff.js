fa7e551981fc68e24c19b1faad7e74a7f365b2b5
mathieu <mathieucrombe@gmail.com>
2021-06-15 21:54:47 +0200

    ajout molten coke brick

---
+++ kubejs/startup_scripts/custom_stuff.js
@@ -0,0 +1,4 @@
+onEvent('fluid.registry', event => {
+    //create molten coke brick
+    event.create('thick_fluid').textureThick(0x292929).bucketColor(0x292929).displayName('Molten Coke Brick')
+})
