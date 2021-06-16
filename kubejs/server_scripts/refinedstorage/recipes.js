e458f8a8f18572d0b45526e5cfcfec314d8adec7
Gogo08 <gogo08190@gmail.com>
2021-06-16 19:56:19 +0200

    Fix Refined

--- kubejs/server_scripts/refinedstorage/recipes.js
+++ kubejs/server_scripts/refinedstorage/recipes.js
@@ -17,134 +17,140 @@ onEvent('recipes', event => {
   event.shapeless('extradisks:raw_withering_processor', ['refinedstorage:processor_binding', 'minecraft:nether_star'])
 
   // Basic Processor
+  event.remove({ output: 'refinedstorage:basic_processor' })
   event.custom(
-      {
-          "type": "appliedenergistics2:inscriber",
-          "mode": "press",
-          "result": {
-              "item": "refinedstorage:basic_processor"
-          },
-          "ingredients": {
-              "top": {
-                  "item": "refinedstorage:raw_basic_processor"
-              },
-              "middle": {
-                  "tag": "minecraft:redstone"
-              },
-              "bottom": {
-                  "item": "appliedenergistics2:printed_silicon"
-              }
-          }
+    {
+      "type": "appliedenergistics2:inscriber",
+      "mode": "press",
+      "result": {
+        "item": "refinedstorage:basic_processor"
+      },
+      "ingredients": {
+        "top": {
+          "item": "refinedstorage:raw_basic_processor"
+        },
+        "middle": {
+          "tag": "forge:dusts/redstone"
+        },
+        "bottom": {
+          "item": "appliedenergistics2:printed_silicon"
+        }
       }
+    }
   )
 
   // Improved Processor
+  event.remove({ output: 'refinedstorage:improved_processor' })
   event.custom(
-      {
-          "type": "appliedenergistics2:inscriber",
-          "mode": "press",
-          "result": {
-              "item": "refinedstorage:improved_processor"
-          },
-          "ingredients": {
-              "top": {
-                  "item": "refinedstorage:raw_improved_processor"
-              },
-              "middle": {
-                  "tag": "minecraft:redstone"
-              },
-              "bottom": {
-                  "item": "appliedenergistics2:printed_silicon"
-              }
-          }
+    {
+      "type": "appliedenergistics2:inscriber",
+      "mode": "press",
+      "result": {
+        "item": "refinedstorage:improved_processor"
+      },
+      "ingredients": {
+        "top": {
+          "item": "refinedstorage:raw_improved_processor"
+        },
+        "middle": {
+          "tag": "forge:dusts/redstone"
+        },
+        "bottom": {
+          "item": "appliedenergistics2:printed_silicon"
+        }
       }
+    }
   )
 
   // Advanced Processor
+  event.remove({ output: 'refinedstorage:advanced_processor' })
   event.custom(
-      {
-          "type": "appliedenergistics2:inscriber",
-          "mode": "press",
-          "result": {
-              "item": "refinedstorage:advanced_processor"
-          },
-          "ingredients": {
-              "top": {
-                  "item": "refinedstorage:raw_advanced_processor"
-              },
-              "middle": {
-                  "tag": "minecraft:redstone"
-              },
-              "bottom": {
-                  "item": "appliedenergistics2:printed_silicon"
-              }
-          }
+    {
+      "type": "appliedenergistics2:inscriber",
+      "mode": "press",
+      "result": {
+        "item": "refinedstorage:advanced_processor"
+      },
+      "ingredients": {
+        "top": {
+          "item": "refinedstorage:raw_advanced_processor"
+        },
+        "middle": {
+          "tag": "forge:dusts/redstone"
+        },
+        "bottom": {
+          "item": "appliedenergistics2:printed_silicon"
+        }
       }
+    }
   )
 
   // Withering Processor
+  event.remove({ output: 'extradisks:withering_processor' })
   event.custom(
-      {
-          "type": "appliedenergistics2:inscriber",
-          "mode": "press",
-          "result": {
-              "item": "extradisks:withering_processor"
-          },
-          "ingredients": {
-              "top": {
-                  "item": 'extradisks:raw_withering_processor'
-              },
-              "middle": {
-                  "tag": "minecraft:redstone"
-              },
-              "bottom": {
-                  "item": "appliedenergistics2:printed_silicon"
-              }
-          }
+    {
+      "type": "appliedenergistics2:inscriber",
+      "mode": "press",
+      "result": {
+        "item": "extradisks:withering_processor"
+      },
+      "ingredients": {
+        "top": {
+          "item": "extradisks:raw_withering_processor"
+        },
+        "middle": {
+          "tag": "forge:dusts/redstone"
+        },
+        "bottom": {
+          "item": "appliedenergistics2:printed_silicon"
+        }
       }
+    }
   )
 
   // Construction Core
+  event.remove({ output: 'refinedstorage:construction_core' })
   event.custom(
-      {
-          "type": "appliedenergistics2:inscriber",
-          "mode": "press",
-          "result": {
-              "item": "refinedstorage:construction_core"
-          },
-          "ingredients": {
-              "top": {
-                  "item": "refinedstorage:raw_basic_processor"
-              },
-              "middle": {
-                  "tag": "minecraft:glowstone_dust"
-              },
-              "bottom": {
-                  "item": "appliedenergistics2:printed_silicon"
-              }
-          }
+    {
+      "type": "appliedenergistics2:inscriber",
+      "mode": "press",
+      "result": {
+        "item": "refinedstorage:construction_core"
+      },
+      "ingredients": {
+        "top": {
+          "item": "refinedstorage:raw_basic_processor"
+        },
+        "middle": {
+          "tag": "forge:dusts/glowstone"
+        },
+        "bottom": {
+          "item": "appliedenergistics2:printed_silicon"
+        }
       }
+    }
   )
 
   // Destruction Core
+  event.remove({ output: 'refinedstorage:destruction_core' })
   event.custom(
-      {
-          "type": "appliedenergistics2:inscriber",
-          "mode": "press",
-          "result": {
-              "item": "refinedstorage:destruction_core"
-          },
-          "ingredients": {
-              "top": {
-                  "item": "refinedstorage:raw_basic_processor"
-              },
-              "middle": {
-                  "tag": "minecraft:quartz"
-              },
-              "bottom": {
-                  "item": "appliedenergistics2:printed_silicon"
-              }
-          }
+    {
+      "type": "appliedenergistics2:inscriber",
+      "mode": "press",
+      "result": {
+        "item": "refinedstorage:destruction_core"
+      },
+      "ingredients": {
+        "top": {
+          "item": "refinedstorage:raw_basic_processor"
+        },
+        "middle": {
+          "tag": "forge:gems/quartz"
+        },
+        "bottom": {
+          "item": "appliedenergistics2:printed_silicon"
+        }
       }
+    }
   )
 });
