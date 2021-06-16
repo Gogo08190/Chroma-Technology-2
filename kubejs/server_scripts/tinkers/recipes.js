e102ea84b0668840d3a0d915b2a55915623a2888
Gogo08 <gogo08190@gmail.com>
2021-06-16 20:08:18 +0200

    Fix Tinkers Recipes

--- kubejs/server_scripts/tinkers/recipes.js
+++ kubejs/server_scripts/tinkers/recipes.js
@@ -1,5 +1,5 @@
 events.listen('recipes', event => {
 
-  event.custom({"type": "thermal:pyrolyzer","ingredient": {"item": "minecraft:blaze_rod"},"result": [{"fluid": "tconstruct:blazing_blood","amount": 20},{"item": 'minecraft:blaze_powder',"chance": 0.50}],"energy": 5000,"experience": 0.00})
+  event.custom({"type": "thermal:pyrolyzer","ingredient": {"item": "minecraft:blaze_rod"},"result": [{"fluid": "tconstruct:blazing_blood","amount": 20},{"item": 'minecraft:blaze_powder',"chance": 0.50}],"energy": 1000,"experience": 0.00})
 
 })
