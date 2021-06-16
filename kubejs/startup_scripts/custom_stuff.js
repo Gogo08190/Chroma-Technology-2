2982c3e50e9a341ec88424107ab46d521bf0578a
mathieu <mathieucrombe@gmail.com>
2021-06-16 22:33:21 +0200

    debug core

--- kubejs/startup_scripts/custom_stuff.js
+++ kubejs/startup_scripts/custom_stuff.js
@@ -9,9 +9,11 @@ onEvent('fluid.registry', event => {
 //Item custom
 
 onEvent('item.registry', event => {
+
     // The texture for this item has to be placed in kubejs/assets/kubejs/textures/item/test_item.png
     event.create('boot').displayName('Boot Core')
     event.create('leging').displayName('Legging Core')
     event.create('Chest').displayName('Chestplate Core')
     event.create('helmet').displayName('Helmet Core')
+    
   })
