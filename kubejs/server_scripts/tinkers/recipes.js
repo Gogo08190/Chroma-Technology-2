c46b0f74b30c964b1f19d8ce4260f39b0de8dacb
Gogo08 <gogo08190@gmail.com>
2021-06-16 23:27:25 +0200

    Fix

--- kubejs/server_scripts/tinkers/recipes.js
+++ kubejs/server_scripts/tinkers/recipes.js
@@ -4,12 +4,14 @@ events.listen('recipes', event => {
 
   //molten shadowsteel
   event.custom({"type": "tconstruct:melting","ingredient": {"item": "create:shadow_steel"},"result": {"fluid": "kubejs:Molten_shadow_steel","amount": 144},"temperature": 800,"time": 50})
+
   //molten to item
   event.custom({"type": "tconstruct:casting_table","cast": {"item": 'tconstruct:ingot_cast' },"cast_consumed": false,"fluid": {"name": "kubejs:Molten_shadow_steel","amount": 144},"result": 'create:shadow_steel',"cooling_time": 100})
 
   //molten coke brick
   event.custom({"type": "tconstruct:alloy","inputs": [{"name": "kubejs:Molten_shadow_steel","amount": 144},{"name": "tconstruct:molten_slimesteel","amount": 576},{"name": "tconstruct:molten_clay","amount": 4000}],
  "result": {"fluid": "kubejs:thick_fluid","amount": 4000},"temperature": 800})
- //molten to block
- event.custom({"type": "tconstruct:casting_basin","fluid": {"name": "kubejs:thick_fluid","amount": 1000},"result": "immersiveengineering:cokebrick","cooling_time": 80})
-})
+
+  //molten to block
+  event.custom({"type": "tconstruct:casting_basin","fluid": {"name": "kubejs:thick_fluid","amount": 1000},"result": "immersiveengineering:cokebrick","cooling_time": 80})
+});
--- kubejs/startup_scripts/custom_stuff.js
+++ kubejs/startup_scripts/custom_stuff.js
@@ -1,21 +1,20 @@
 
-//fluid custom
+// Fluid custom
 
 onEvent('fluid.registry', event => {
     //create molten coke brick
     event.create('thick_fluid').textureThick(0x292929).bucketColor(0x292929).displayName('Molten Coke Brick')
     //create molten shadow steel
-    event.create('Molten_shadow_steel').textureThick(0x4B006E).bucketColor(0x4B006E).displayName('Molten shadow steel')
+    event.create('molten_shadow_steel').textureThick(0x4B006E).bucketColor(0x4B006E).displayName('Molten shadow steel')
 })
 
-//Item custom
+// Item custom
 
 onEvent('item.registry', event => {
 
     // The texture for this item has to be placed in kubejs/assets/kubejs/textures/item/test_item.png
     event.create('boot').displayName('Boot Core')
     event.create('leging').displayName('Legging Core')
-    event.create('Chest').displayName('Chestplate Core')
+    event.create('chest').displayName('Chestplate Core')
     event.create('helmet').displayName('Helmet Core')
-
-  })
+})
