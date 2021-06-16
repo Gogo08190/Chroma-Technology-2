3a87cd30696e86aafc1db2f75a003f3e803cdea2
mathieu <mathieucrombe@gmail.com>
2021-06-16 22:12:07 +0200

    ajout nouveaux item (core)

--- kubejs/startup_scripts/custom_stuff.js
+++ kubejs/startup_scripts/custom_stuff.js
@@ -1,4 +1,17 @@
+
+//fluid custom
+
 onEvent('fluid.registry', event => {
     //create molten coke brick
     event.create('thick_fluid').textureThick(0x292929).bucketColor(0x292929).displayName('Molten Coke Brick')
 })
+
+//Item custom
+
+onEvent('item.registry', event => {
+    // The texture for this item has to be placed in kubejs/assets/kubejs/textures/item/test_item.png
+    event.create('boot').displayName('Boot Core')
+    event.create('leging').displayName('Legging Core')
+    event.create('Chest').displayName('Chestplate Core')
+    event.create('helmet').displayName('Helmet Core')
+  })
