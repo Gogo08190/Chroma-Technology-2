3354fa9900ae407a0933fac98833299481b5a170
Gogo08 <gogo08190@gmail.com>
2021-06-21 17:48:43 +0200

    Overload / Productivebees Craft

--- kubejs/server_scripts/productivebees/comb.js
+++
@@ -1,4 +0,0 @@
-events.listen('recipes', event => {
-
-  event.recipes.thermal.centrifuge([Item.of('mythicbotany:alfsteel_nugget', 12), Fluid.of('productivebees:honey', 400)], 'productivebees:comb_alfsteel')
-});
