761b6135fd74183408b05921bfc5b7b244c05ff4
koepss <pierrelode2@outlook.fr>
2021-06-23 23:36:11 +0200

    craft t5 blood magic

--- kubejs/server_scripts/bloodmagic/recipes.js
+++ kubejs/server_scripts/bloodmagic/recipes.js
@@ -1,5 +1,5 @@
 onEvent('recipes', event => {
 
   event.recipes.bloodmagic.alchemytable('kubejs:pure_evil', ['upgradednetherite:ultimate_upgraded_netherite_ingot', 'botania:cosmetic_devil_horns', 'druidcraft:red_soulfire', 'bloodmagic:demonslate', 'botania:cosmetic_devil_tail', 'kubejs:chroma_ingot']).syphon(666).upgradeLevel(2)
-
+  event.recipes.bloodmagic.altar( 'bloodmagic:etherealslate', 'bloodmagic:demonslate').upgradeLevel(5).altarSyphon(30000).consumptionRate(35).drainRate(40)
 })
