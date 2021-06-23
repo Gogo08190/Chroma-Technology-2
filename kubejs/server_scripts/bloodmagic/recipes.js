6cf4cf9d595b2355fb0e7e863c23bf0e8aabf088
Gogo08 <gogo08190@gmail.com>
2021-06-23 23:23:27 +0200

    Craft custom bloodmagic

---
+++ kubejs/server_scripts/bloodmagic/recipes.js
@@ -0,0 +1,5 @@
+onEvent('recipes', event => {
+
+  event.recipes.bloodmagic.alchemytable('kubejs:pure_evil', ['upgradednetherite:ultimate_upgraded_netherite_shield', 'botania:cosmetic_devil_horns', 'druidcraft:red_soulfire', 'bloodmagic:demonslate', 'botania:cosmetic_devil_tail', 'kubejs:chroma_ingot']).syphon(666).upgradeLevel(2)
+
+})
