c84e4ffe40d180250664d0d0769a0fe52068eaa4
Gogo08 <gogo08190@gmail.com>
2021-06-28 22:55:49 +0200

    Time In A Bottle recipe

--- kubejs/server_scripts/custom/recipes.js
+++ kubejs/server_scripts/custom/recipes.js
@@ -1,8 +1,23 @@
 events.listen('recipes', event => {
 
+  // Unassembled Chroma Ingot
   event.recipes.thermal.smelter(Item.of('kubejs:unassembled_chroma_ingot', 8), ['silentgear:blueprint_paper', 'lazierae2:spec_core_64', 'botania:terrasteel_ingot']).energy(10000)
 
   event.recipes.thermal.smelter(Item.of('kubejs:unassembled_chroma_ingot', 4), ['silentgear:blueprint_paper', 'lazierae2:spec_core_32', 'botania:terrasteel_ingot']).energy(7000)
 
   event.recipes.thermal.smelter('kubejs:unassembled_chroma_ingot', ['silentgear:blueprint_paper', 'lazierae2:spec_core_16', 'botania:terrasteel_ingot']).energy(5000)
+
+  // Time In A Bottle
+  event.remove({ output: 'tiab:timeinabottle' })
+  event.shaped('tiab:timeinabottle', [
+  'GGG',
+  'DCD',
+  'LBL'
+  ], {
+    G: '#forge:ingots/gold',
+    D: 'tombstone:impregnated_diamond',
+    C: 'create:cuckoo_clock',
+    L: 'create:lapis_sheet',
+    B: 'minecraft:glass_bottle'
+  })
 });
