6fa9904ffef84ff1c7695bf5244ee163f8f6460e
Gogo08 <gogo08190@gmail.com>
2021-07-20 18:13:29 +0200

    Fix craft

--- kubejs/server_scripts/bloodmagic/recipes.js
+++ kubejs/server_scripts/bloodmagic/recipes.js
@@ -13,7 +13,7 @@ onEvent('recipes', event => {
   })
 
   // Pure Evil
-  event.recipes.bloodmagic.alchemytable('kubejs:pure_evil', ['upgradednetherite:ultimate_upgraded_netherite_ingot', 'botania:cosmetic_devil_horns', 'druidcraft:red_soulfire', 'bloodmagic:demonslate', 'botania:cosmetic_devil_tail', 'kubejs:chroma_ingot']).syphon(666).upgradeLevel(2)
+  event.recipes.bloodmagic.alchemytable('kubejs:pure_evil', ['upgradednetherite:ultimate_upgraded_netherite_ingot', 'botania:cosmetic_devil_horns', 'druidcraft:red_soulfire', 'bloodmagic:demonslate', 'botania:cosmetic_devil_tail', 'bloodmagic:weakbloodshard']).syphon(666).upgradeLevel(2)
 
   // Ethereal Slate
   event.recipes.bloodmagic.altar( 'bloodmagic:etherealslate', 'bloodmagic:demonslate').upgradeLevel(4).altarSyphon(30000).consumptionRate(35).drainRate(40)
