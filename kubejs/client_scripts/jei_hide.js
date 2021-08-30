d7aa9f16218eac35541f54dbc7002bfc79bda97e
Gogo08 <gogo08190@gmail.com>
2021-08-30 21:38:30 +0200

    Fix config

--- kubejs/client_scripts/jei_hide.js
+++ kubejs/client_scripts/jei_hide.js
@@ -103,6 +103,7 @@ events.listen('jei.hide.items', function (event) {
   event.hide('iceandfire:amythest_ore')
   event.hide('iceandfire:amythest_block')
   event.hide('iceandfire:amythest_gem')
+  event.hide(Item.of('iceandfire:gorgon_head', {Damage:0}))
 
 
   // Silent Gear
