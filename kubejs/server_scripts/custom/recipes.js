5d74ca8bd988da154a49c650be6baed5c0f3fd32
Gogo08 <gogo08190@gmail.com>
2021-10-18 18:31:07 +0200

    Add furnace recipe

--- kubejs/server_scripts/custom/recipes.js
+++ kubejs/server_scripts/custom/recipes.js
@@ -132,4 +132,8 @@ events.listen('recipes', event => {
     O: 'minecraft:obsidian',
     P: 'powah:aerial_pearl'
   })
+
+  event.smelting('thermal:lead_ingot', 'thermal:lead_dust')
+  event.smelting('silents_mechanisms:uranium_dust', 'silents_mechanisms:uranium_ingot')
+  event.smelting('mekanism:uranium_ore', 'silents_mechanisms:uranium_ingot')
 });
