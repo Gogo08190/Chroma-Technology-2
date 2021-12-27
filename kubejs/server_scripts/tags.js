d688e958eaa9c34facb9c2f64b18857794f5e412
koepss <pierrelode2@outlook.fr>
2021-12-27 15:16:20 +0100

    Fix tag Productive Bee

--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -1,4 +1,4 @@
 // priority: 100
 onEvent("item.tags", (event) => {
-    event.add("itemfilters:check_nbt", ["extendedcrafting:singularity", "patchouli:guide_book", "immersiveengineering:blueprint"]);
+    event.add("itemfilters:check_nbt", ["extendedcrafting:singularity", "patchouli:guide_book", "immersiveengineering:blueprint", "productivebees:configurable_comb", "productivebees:spawn_egg_configurable_bee", "productivebees:honey_treat", "productivebees:gene"]);
 });
