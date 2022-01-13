47dede479342995d015d86db21e8279b63b472bf
Gogo08 <gogo08190@gmail.com>
2022-01-13 15:52:49 +0100

    Add woot key recipes

---
+++ kubejs/server_scripts/woot/recipes.js
@@ -0,0 +1,48 @@
+onEvent('recipes', event => {
+
+  event.shaped('woot:exotic_a', [
+  'GHG',
+  'HLH',
+  'GHG'
+  ], {
+    G: 'woot:machine_casing',
+    H: 'woot:cell_4',
+    L: 'woot:efficiency_3'
+  })
+  event.shaped('woot:exotic_b', [
+  'GHG',
+  'HLH',
+  'GHG'
+  ], {
+    G: 'woot:machine_casing',
+    H: 'woot:cell_4',
+    L: 'woot:tier_shard_3'
+  })
+  event.shaped('woot:exotic_c', [
+  'GHG',
+  'HLH',
+  'GHG'
+  ], {
+    G: 'woot:machine_casing',
+    H: 'woot:cell_4',
+    L: 'woot:looting_3'
+  })
+  event.shaped('woot:exotic_d', [
+  'GHG',
+  'HLH',
+  'GHG'
+  ], {
+    G: 'woot:machine_casing',
+    H: 'woot:cell_4',
+    L: 'woot:rate_3'
+  })
+  event.shaped('woot:exotic_e', [
+  'GHG',
+  'HLH',
+  'GHG'
+  ], {
+    G: 'woot:machine_casing',
+    H: 'woot:cell_4',
+    L: 'woot:mass_3'
+  })
+});
